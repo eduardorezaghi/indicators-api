838beefdda654c1e8b0c2199bf81da90e9471332
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2025-01-15 12:38:49 -0300

    chore: Adjust pytest configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 from dotenv import load_dotenv
 from flask import Flask
 from flask.testing import FlaskClient
-from flask_sqlalchemy import SQLAlchemy
 
 from src.config import Settings
 from src.database import destroy_db, init_db
@@ -19,13 +18,6 @@ test_settings = Settings(
 )
 
 
-@pytest.fixture(scope="session")
-def test_db() -> SQLAlchemy:
-    return SQLAlchemy(
-        engine_options={"echo": test_settings.DB_ECHO_LOG},
-    )
-
-
 class CustomClient(FlaskClient):
     def open(self, *args, **kwargs):  # type: ignore
         kwargs.setdefault("content_type", "application/json")
@@ -38,15 +30,15 @@ def default_app() -> Flask:
 
 
 @pytest.fixture(scope="session", autouse=True)
-def app(test_db: SQLAlchemy) -> Generator[Flask, None, None]:
-    app = create_app(settings_dict=test_settings, db=test_db)
+def app() -> Generator[Flask, None, None]:
+    app = create_app(settings_dict=test_settings)
 
     with app.app_context():
-        init_db(test_db)
+        init_db()
         # Custom test client with default content type set to application/json.
         app.test_client_class = CustomClient
         yield app
-        destroy_db(test_db)
+        destroy_db()
 
 
 @pytest.fixture
